e87ff2c4d84013accdb2247d34afdef8efb4bd8a
Zakariás Adrienn <vub8vd@inf.elte.hu>
2020-03-09 12:53:26 +0100

    Update AtlantisDefense/src/atlantisdefense/AtlantisDefense.java

--- AtlantisDefense/src/atlantisdefense/AtlantisDefense.java
+++ AtlantisDefense/src/atlantisdefense/AtlantisDefense.java
@@ -16,7 +16,7 @@ public class AtlantisDefense {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int valami;
+        int valami = 7;
     }
     
 }
