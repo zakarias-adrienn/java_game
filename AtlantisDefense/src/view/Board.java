4e9bb211c663f4aaa34946ef319f187475388194
Zakariás Adrienn <zakarias.adrienn@yahoo.com>
2020-03-19 13:02:42 +0100

    Életerő sávos tornyokat tudunk lehelyezni

--- AtlantisDefense/src/view/Board.java
+++ AtlantisDefense/src/view/Board.java
@@ -1,14 +1,20 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,13 +27,13 @@ import model.BubbleTower;
 import model.ElectricTower;
 import model.IceTower;
 
-
 public class Board extends JPanel {
 
     public final Image sand, route, towerPlace, ice, bubble, gold, electric;
     public Cell[][] cells;
     public static ArrayList<Cell> routeCells;
     int spotIndex = 0;
+    
 
     public Board() throws IOException {
         sand = ResourceLoader.loadImage("res/homok.png");
@@ -63,58 +69,55 @@ public class Board extends JPanel {
                 ImageIcon icon = new ImageIcon(img);
                 Cell thumb = new Cell(icon, y, x);
                 thumb.setIcon(icon);
-                if(r){
+                if (r) {
                     routeCells.add(thumb);
                 }
                 cells[y][x] = thumb;
-                cells[y][x].setSize(58,58);
-                
+                cells[y][x].setSize(58, 58);
+
                 if (img == towerPlace) {
                     thumb.addMouseListener(new MouseAdapter() {
                         @Override
                         public void mouseClicked(MouseEvent e) {
                             System.out.println("he");
                             int imageNumber = View.checkClick();
-                            if(imageNumber!=0){
+                            if (imageNumber != 0) {
                                 ImageIcon icon;
                                 Tower tmp;
-                                if(imageNumber==1){
+                                if (imageNumber == 1) {
                                     icon = new ImageIcon(gold);
                                     tmp = new GoldTower();
-                                }
-                                else if(imageNumber==2){
+                                } else if (imageNumber == 2) {
                                     icon = new ImageIcon(bubble);
                                     tmp = new BubbleTower();
-                                }
-                                else if(imageNumber==3){
+                                } else if (imageNumber == 3) {
                                     icon = new ImageIcon(electric);
                                     tmp = new ElectricTower();
-                                }
-                                else {
+                                } else {
                                     icon = new ImageIcon(ice);
                                     tmp = new IceTower();
                                 }
-                                if (Model.money - tmp.price >= 0)
-                                {
-                                thumb.setIcon(icon);
-                                Model.money -= tmp.price;
-                                System.out.println(Model.money);
-                                View.moneyView.setText(Integer.toString(Model.money));
-                                Model.towers.add(spotIndex, tmp);
-                                System.out.println(Model.towers);
-                                spotIndex++;
+                                if (Model.money - tmp.price >= 0) {
+                                    thumb.setIcon(icon);
+                                    thumb.toronyLe = true;
+                                    Model.money -= tmp.price;
+                                    System.out.println(Model.money);
+                                    View.moneyView.setText(Integer.toString(Model.money));
+                                    Model.towers.add(spotIndex, tmp);
+                                    System.out.println(Model.towers);
+                                    spotIndex++;
                                 }
                             }
                         }
                     });
-                    
+
                 }
                 this.add(thumb);
             }
         }
     }
-    
-    public static void enemyComes(Image img, int speed){
+
+    public static void enemyComes(Image img, int speed) {
         // itt kellene haladjon az enemy adott sebességgel a routeCells cellákon
         // a sebessége fgvében haladjon végig az úton -> routeCells celláinak ikonjait kell lecserélni
         ImageIcon icon = new ImageIcon(img);
--- AtlantisDefense/src/view/Cell.java
+++ AtlantisDefense/src/view/Cell.java
@@ -1,17 +1,30 @@
 package view;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import res.ResourceLoader;
 
 public class Cell extends JLabel {
     private ImageIcon icon;
     private int xPos, yPos;
     public int majom;
+    public Image img;
+    public boolean toronyLe = false;
     
     Cell(ImageIcon icon, int x, int y){
         this.icon = icon;
         this.xPos = x;
         this.yPos = y;
+        try {
+            img = ResourceLoader.loadImage("res/hb100.png");
+        } catch (IOException ex) {
+            System.out.println("Kép betöltés sikertelen!");
+        }
     }
     
     public int getXPos(){
@@ -21,6 +34,21 @@ public class Cell extends JLabel {
     public int getYPos(){
         return yPos;
     }
+    
+    
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponents(g);
+        if(toronyLe){
+            super.paintComponent(g);
+            g.drawImage(img, 0, 0, null);
+        } else {
+            super.paintComponent(g);
+        }
+        
+    }
+    
+    
     
     
 }
