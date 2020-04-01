4fb98f37efb4c782ec5a237d07ed3b6ae2ebe50f
Wolosz András <pqinmc@inf.elte.hu>
2020-04-02 00:16:02 +0200

    Valamiféle ütés grafikusan is megjelenik. Nyers

--- AtlantisDefense/src/model/Enemy.java
+++ AtlantisDefense/src/model/Enemy.java
@@ -1,47 +1,56 @@
 package model;
 
+import java.awt.Color;
 import java.awt.Point;
 import java.util.ArrayList;
+import javax.swing.JLabel;
+import view.Menu;
 
 public abstract class Enemy {
+
     protected Point pos; // x, y helyett
     protected int life;
     protected int strength;
     protected int speed;
     protected String type;
-    
+
     public abstract String getType();
-    
+
     public abstract void move();
-    
+
     public Point collosion() {
         //System.out.println("" + (116 + 28 + 57 * (pos.y - 1)) + ", " + (100 + 23 + 47 * (pos.x - 1)));
         //pos = new Point(116 + 28 + 57 * (towerY - 1), 100 + 23 + 47 * (towerX - 1));
-        
-        Point enemyAbsoulutePos = new Point((116 + 28 + 57 * (pos.y - 1)), (100 + 23 + 47 * (pos.x - 1))); 
+
+        int precision = 10;
+        Point enemyAbsoulutePos = new Point((116 + 28 + 57 * (pos.y - 1)), (100 + 23 + 47 * (pos.x - 1)));
         System.out.println(Model.allBullets.size());
-        for (int i = 0; i<Model.allBullets.size(); i++)
-        {
-            if (Math.abs(Model.allBullets.get(i).pos.x-enemyAbsoulutePos.x) < 15 && Math.abs(Model.allBullets.get(i).pos.y-enemyAbsoulutePos.y) < 15)
-            {
+        for (int i = 0; i < Model.allBullets.size(); i++) {
+            if (Math.abs(Model.allBullets.get(i).pos.x - enemyAbsoulutePos.x) < precision && Math.abs(Model.allBullets.get(i).pos.y - enemyAbsoulutePos.y) < precision) {
                 System.out.println("HIT");
+                JLabel bullet = new javax.swing.JLabel();
+                bullet.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/hit.png")));
+                Menu.v.getContentPane().add(bullet);
+                bullet.setBounds(98, 54, 30, 30);
+                bullet.setForeground(Color.WHITE);
+                Menu.v.getContentPane().setComponentZOrder(bullet, 0);
+                bullet.setLocation(enemyAbsoulutePos.x, enemyAbsoulutePos.y);
             }
         }
-        return new Point((116 + 28 + 57 * (pos.y - 1)), (100 + 23 + 47 * (pos.x - 1))); 
+        return new Point((116 + 28 + 57 * (pos.y - 1)), (100 + 23 + 47 * (pos.x - 1)));
     }
-    
-    public void setPos(int x, int y){
+
+    public void setPos(int x, int y) {
         this.pos.x = x;
         this.pos.y = y;
     }
-    
-    public int getXPos(){
+
+    public int getXPos() {
         return this.pos.x;
     }
-    
-    public int getYPos(){
+
+    public int getYPos() {
         return this.pos.y;
     }
-    
-    
+
 }
