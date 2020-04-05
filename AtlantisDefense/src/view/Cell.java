474ef35de4f09c0d22944ec0c8e21970970fb041
Zakariás Adrienn <zakarias.adrienn@yahoo.com>
2020-04-05 11:29:20 +0200

    Hibajavítás - torony eladása után is a helyén toronyhely marad

--- AtlantisDefense/src/view/Cell.java
+++ AtlantisDefense/src/view/Cell.java
@@ -26,9 +26,9 @@ public class Cell extends JLabel {
     public void getImage() throws IOException {
         if(this.life==-1){
             this.img = null;
-        }else if (this.life == 0 && this.isTower) {
+        }else if (this.life == 0 && (this.isTower || this.wasJustPlaced)) {
             this.img = ResourceLoader.loadImage("res/toronyhely.png");
-        } else if (this.life == 0 && !this.isTower) {
+        } else if (this.life == 0) {
             this.img = ResourceLoader.loadImage("res/ut.png");
         } else if (this.life > 0 && this.life < 10) {
             this.img = ResourceLoader.loadImage("res/hb0.png");
