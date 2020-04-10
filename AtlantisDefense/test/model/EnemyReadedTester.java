d25dc70829be4595c8df79f72a7e8cd62a2dce41
Zakariás Adrienn <zakarias.adrienn@yahoo.com>
2020-04-10 13:07:25 +0200

    EnemyReaded teszt - nem működik

--- AtlantisDefense/test/model/EnemyReadedTester.java
+++ AtlantisDefense/test/model/EnemyReadedTester.java
@@ -1,11 +1,14 @@
 package model;
 
+import java.awt.Image;
+import java.io.IOException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import res.ResourceLoader;
 
 
 public class EnemyReadedTester {
@@ -35,5 +38,22 @@ public class EnemyReadedTester {
     // @Test
     // public void hello() {}
     
-    // kell a getImage és a konstruktor tesztjei
+    @Test
+    public void getImageTest() throws IOException{
+        Image gold, electric, ice, bubble;
+        gold = ResourceLoader.loadImage("res/goldfish_small.png");
+        electric = ResourceLoader.loadImage("res/eel_small.png");
+        ice = ResourceLoader.loadImage("res/swordfish_small.png");
+        bubble = ResourceLoader.loadImage("res/fugu_small.png");
+        EnemyReaded e = new EnemyReaded("electric", 100, 200);
+        // hogy lehet képek EGYENLŐSÉGÉT VIZSGÁLNI?
+//        assertTrue("Image should be electric", e.getImage()==electric);
+//        e = new EnemyReaded("ice", 100, 200);
+//        assertEquals("Image should be ice", e.getImage(), ice);
+//        e = new EnemyReaded("gold", 100, 200);
+//        assertEquals("Image should be gold", e.getImage(), gold);
+//        e = new EnemyReaded("bubble", 100, 200);
+//        assertEquals("Image should be bubble", e.getImage(), bubble);
+//        
+    }
 }
